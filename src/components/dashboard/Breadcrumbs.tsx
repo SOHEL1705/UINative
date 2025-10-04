1b413d871e81c9402a70997a616af5c2097e8e03
AnsaRiSohel <sonudhonu@gmail.com>
2025-10-04 15:26:39 +0530

    asd

---
+++ src/components/dashboard/Breadcrumbs.tsx
@@ -0,0 +1,17 @@
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "../ui/breadcrumb";
+export function Breadcrumbs() {
+  return <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>;
+}
+  
